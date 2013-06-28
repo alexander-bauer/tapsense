51cd964360b11d02f31b345e8ae5794fbeec104c
Sasha Crofter <sasha@crofter.org>
2013-06-27 21:07:06 -0400

    Add settings architecture

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import signal
 import sys
 
 import taptester
+import settings
 
 # Listen for SIGINT and exit gracefully.
 def sigint_handler(signal, frame):
@@ -15,11 +16,7 @@ signal.signal(signal.SIGINT, sigint_handler)
 # Set up a TapTester, and calibrate if possible.
 tt = taptester.TapTester()
 
-# Define the function that will be called on tap.
-def onTap():
-    print 'tap'
-
-tt.onTap = onTap
+tt.onTap = settings.onTap
 
 # Listen forever, or until terminated by SIGINT.
 while True:
