c0941af6fe2e90883f55bc81fae6986434761ed1
Sasha Crofter <sasha@crofter.org>
2013-06-27 21:57:39 -0400

    Add a sensible extension architecture
    Also add a default onTap() function which prints 'Tap!' when it detects
    a tap.

--- main.py
+++ main.py
@@ -16,7 +16,16 @@ signal.signal(signal.SIGINT, sigint_handler)
 # Set up a TapTester, and calibrate if possible.
 tt = taptester.TapTester()
 
-tt.onTap = settings.onTap
+
+# If settings.onTap is defined, make it the callback.
+if callable(settings.onTap):
+    tt.onTap = settings.onTap
+else:
+    # Use this simple function as a default.
+    def onTap():
+        print 'Tap!'
+    tt.onTap = onTap
+
 
 # Listen forever, or until terminated by SIGINT.
 while True:
