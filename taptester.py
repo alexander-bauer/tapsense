d7ef8c6eb16a50649ea92bf7b1f58a1d7fcef27b
Sasha Crofter <sasha@crofter.org>
2013-06-28 09:50:29 -0400

    Simplify output somewhat

--- taptester.py
+++ taptester.py
@@ -59,16 +59,14 @@ class TapTester(object):
         device_index = None            
         for i in range( self.pa.get_device_count() ):     
             devinfo = self.pa.get_device_info_by_index(i)   
-            print( "Device %d: %s"%(i,devinfo["name"]) )
 
             for keyword in ["pulse","mic","input"]:
                 if keyword in devinfo["name"].lower():
-                    print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
                     device_index = i
                     return device_index
 
         if device_index == None:
-            print( "No preferred input found; using default input device." )
+            print 'No preferred input found; using default input device.'
 
         return device_index
 
